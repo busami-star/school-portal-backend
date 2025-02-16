d301d281df3348a29cbf8313caafd38fa355ab3e
Busami Victor <victorbusami1@gmail.com>
2025-02-16 11:11:57 +0300

    routes commit

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import studentRoutes from "./routes/students.routes";
+import teacherRoutes from "./routes/teachers.routes";
+import courseRoutes from "./routes/courses.routes";
+import authRoutes from "./routes/auth.routes";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, Express with TypeScript!");
-});
+app.use("/students", studentRoutes);
+app.use("/teachers", teacherRoutes);
+app.use("/courses", courseRoutes);
+app.use("/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
